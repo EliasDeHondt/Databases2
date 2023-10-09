99836f6ecf8c14be0fc08a00cfd00477f9226327
EliasDeHondt <Elias.dehondt@outlook.com>
2023-10-09 17:28:44 +0200

    Commit

--- Demo/Console_For_Demo.sql
+++ Demo/Console_For_Demo.sql
@@ -50,3 +50,71 @@ UPDATE emp e SET sal = sal * 1.1 WHERE sal < (SELECT AVG(sal) FROM emp WHERE dep
 CREATE USER praktijk PASSWORD 'ROOT123456789' SUPERUSER;
 
 CREATE USER theorie PASSWORD 'ROOT123456789' SUPERUSER;
+
+-- INFO over VACUUM
+CREATE TABLE testTabel( id int ) WITH (AUTOVACUUM_ENABLED = OFF); -- AUTOVACUUM_ENABLED = ON is default
+INSERT INTO testTabel SELECT id FROM generate_series(1, 100000) AS id; -- 100000 rijen
+SELECT * FROM testTabel;
+SELECT PG_SIZE_PRETTY( PG_TOTAL_RELATION_SIZE('testTabel') ); -- Hoeveel ruimte neemt de tabel in
+UPDATE testTabel SET id = id + 1;
+SELECT PG_SIZE_PRETTY( PG_TOTAL_RELATION_SIZE('testTabel') );
+VACUUM testTabel;
+SELECT PG_SIZE_PRETTY( PG_TOTAL_RELATION_SIZE('testTabel') );
+VACUUM FULL testTabel; -- Alle kolommen van de tabel worden opnieuw geschreven
+VACUUM testTabel ( id ); -- Alleen de kolom id
+
+-- INFO over TABLESPACE
+CREATE TABLESPACE testTabelSpace LOCATION 'C:\Users\Gebruiker\Documents\testTabelSpace'; -- Locatie van de tabel
+CREATE TABLE testTabel2 ( id int ) TABLESPACE testTabelSpace; -- Tabel in de tabelspace
+
+-- INFO over ALTER TABLE
+-- Wat kan je doen met ALTER TABLE
+ALTER TABLE testTabel ADD COLUMN naam varchar(255); -- Kolom toevoegen
+ALTER TABLE testTabel ALTER COLUMN naam SET NOT NULL; -- Kolom verplicht maken
+ALTER TABLE testTabel ALTER COLUMN naam DROP NOT NULL; -- Kolom niet meer verplicht maken
+ALTER TABLE testTabel ALTER COLUMN naam SET DEFAULT 'test'; -- Default waarde geven aan kolom
+ALTER TABLE testTabel ALTER COLUMN naam DROP DEFAULT; -- Default waarde verwijderen
+ALTER TABLE testTabel ALTER COLUMN naam TYPE varchar(255); -- Type van kolom veranderen
+ALTER TABLE testTabel ALTER COLUMN naam SET STATISTICS 1000; -- Statistieken van kolom veranderen
+ALTER TABLE testTabel ALTER COLUMN naam SET STORAGE PLAIN; -- Opslag van kolom veranderen (PLAIN, EXTERNAL, EXTENDED, MAIN)
+
+-- Ik wil in tabel emp de kolom deptno 30 naar 35
+UPDATE emp SET deptno = 35 WHERE deptno = 30;
+-- Dit kan niet want er is een foreign key constraint op deptno dus
+UPDATE dept SET deptno = 35 WHERE deptno = 30;
+-- Dit kan ook niet want er is een foreign key constraint op deptno dus dus TRIIGER
+ALTER TABLE emp DISABLE TRIGGER all; -- Alle triggers uitschakelen
+UPDATE emp SET deptno = 35 WHERE deptno = 30;
+
+ALTER TABLE dept DISABLE TRIGGER all; -- Alle triggers uitschakelen
+UPDATE dept SET deptno = 35 WHERE deptno = 30;
+
+ALTER TABLE emp ENABLE TRIGGER all; -- Alle triggers inschakelen
+ALTER TABLE dept ENABLE TRIGGER all; -- Alle triggers inschakelen
+SELECT * FROM emp;
+
+
+UPDATE dept SET dname='sales' WHERE dname='SALES';
+SELECT * FROM dept;
+-- Maak hier een constraint voor aan alles moet in hoofdletters
+ALTER TABLE dept ADD CONSTRAINT dname_uppercase CHECK (dname = UPPER(dname)) NOT VALID; -- NOT VALID is om de constraint niet te laten werken
+UPDATE dept SET dname=UPPER(dname); -- Zet alles in hoofdletters
+ALTER TABLE dept VALIDATE CONSTRAINT dname_uppercase; -- VALIDATE is om de constraint te laten werken
+
+-- Wat is een synonym en hoe maak je die aan in PGadmin
+CREATE VIEW testView AS SELECT * FROM dept; -- Maak een view aan
+
+-- Sequence
+INSERT INTO emp(empno, ename, job, sal, deptno) VALUES ((SELECT max(empno) FROM emp) + 1, 'TEST', 'TEST', 1000, 35); -- Voeg een rij toe aan de tabel
+SELECT * FROM emp ORDER BY empno DESC; -- Bekijk de tabel
+
+CREATE SEQUENCE empSequence START 7935; -- Maak een sequence aan
+SELECT NEXTVAL('empSequence'); -- Geef de volgende waarde van de sequence
+SELECT CURRVAL('empSequence'); -- Geef de huidige waarde van de sequence
+SELECT SETVAL('empSequence', 100); -- Zet de waarde van de sequence op 100
+
+INSERT INTO emp(empno, ename, job, sal, deptno) VALUES (NEXTVAL('empSequence'), 'TEST', 'TEST', 1000, 35);
+
+-- Identity Column
+CREATE TABLE students ( id int GENERATED ALWAYS AS IDENTITY ); -- Maak een tabel aan met een identity column
+
